880268c42988baac41e4079647b5e6b639ecfa3e
DeLesslin George-Warren <delesslingw@gmail.com>
2025-06-08 22:15:39 -0400

    Change hello world text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! We now have an auto-start script");
 });
 
 app.listen(3000, (e) => {
